7345d180bf40dc1c2fd0716012855474bb8decc4
arias0226 <168589678+arias0226@users.noreply.github.com>
2024-05-17 16:23:29 -0500

    Add files via upload
    initial commits

---
+++ index.js
@@ -0,0 +1,13 @@
+//alert("Hola mundo");
+title = document.getElementById('title');
+action = document.getElementById('action');
+image = document.getElementById('image');
+image2 = document.getElementById('image2');
+window.onload = inicializarEventos
+
+action.addEventListener('click', doAction);
+
+async function doAction(){
+    title.innerHTML = "Loading..."
+ç
+}
